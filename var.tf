e1c2dda8870bfd2da7f02b19598cfc1073868719
Deepali karad <dipakarad@gmail.com>
2022-03-27 18:20:24 +0530

    laptop

---
+++ var.tf
@@ -0,0 +1,9 @@
+variable "region"{
+  default = "us-east-1"
+}
+variable "access_key"{
+default = "AKIASU3MY5FO4JSDT3WT"
+}
+variable "secret_key"{
+default = "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"
+}
